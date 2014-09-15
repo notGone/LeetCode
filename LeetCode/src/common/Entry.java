fd54c52975420a1aa34a2ef8eef3f24e882228ea
notGone <notGone@qq.com>
2014-09-15 19:28:59 +0800

    add

--- LeetCode/src/common/Entry.java
+++ LeetCode/src/common/Entry.java
@@ -1,13 +1,17 @@
 package common;
 
 import solutions.Problem_01;
+import solutions.Problem_02;
+import solutions.Problem_02_n;
 
 public class Entry {
   
   public static void main(String[] args){
     System.out.println("--- LeetCode OJ ---");
-    System.out.println(Problem_01.reverseWords("  "));
+//    System.out.println(Problem_01.reverseWords("  "));
 //    System.out.println(Problem_01.reverseWords("the    sky is blue"));
+    String[] tokens = {"4","13","5","/","+"};
+    System.out.println(new Problem_02_n().evalRPN(tokens));
   }
 
 }
